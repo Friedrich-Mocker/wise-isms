5dcb1775542a5fc590759ae6bf25db2c94fc0915
friedrich.mocker <info@mocker-it.de>
2024-04-10 22:53:38 +0200

    lint

--- apps/wise-isms/src/app/nx-welcome.component.ts
+++ apps/wise-isms/src/app/nx-welcome.component.ts
@@ -1,26 +1,24 @@
-import { Component, ViewEncapsulation, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
 import { SharedDataType } from '@wise-isms/shared';
 
 @Component({
   selector: 'app-nx-welcome',
   standalone: true,
   imports: [CommonModule, HttpClientModule],
-  template: `
-  {{backendData | json}}
-   `,
+  template: ` {{ backendData | json }} `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
 export class NxWelcomeComponent {
-  backendData?: SharedDataType
+  backendData?: SharedDataType;
   httpClient = inject(HttpClient);
   constructor() {
-    this.httpClient.get<SharedDataType>('/api').subscribe(
-     { next: (data) => {
+    this.httpClient.get<SharedDataType>('/api').subscribe({
+      next: (data) => {
         this.backendData = data;
-      }}
-    );
+      },
+    });
   }
 }
